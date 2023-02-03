7a3d7882a089ac58539ce98507d62c9a8f71f51e
masouma <masouma2020@gmail.com>
2023-02-03 22:59:18 +0430

    setup routes

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  describe 'Validations For the Like model' do
+    before(:each) do
+      @like = Like.new(user_id: 4, post_id: 10)
+    end
+
+    before { @like }
+
+    it ' User_id should not be blank' do
+      @like.user_id = nil
+      expect(@like).to_not be_valid
+    end
+
+    it ' Post_id should not be blank ' do
+      @like.post_id = nil
+      expect(@like).to_not be_valid
+    end
+  end
+end
