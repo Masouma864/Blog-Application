03dd1887b6ef641fa0422184ddc643f5ba45db7b
masouma <masouma2020@gmail.com>
2023-02-11 09:08:01 +0430

    update route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  root "users#index"
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index", as: :start
+
   get 'users/:id' => 'users#show', as: :user_id
+
+  # Posts
   get 'users/:user_id/posts' => 'posts#index'
+  get 'users/:user_id/posts/new' => 'posts#new', as: :post_new
+  post 'users/:user_id/posts/new' => 'posts#create', as: :post
   get 'users/:user_id/posts/:id' => 'posts#show', as: :post_id
+
+  #Comments
+
+  get 'users/:user_id/posts/:id/comments/new' => 'comments#new', as: :comments_new
+  post 'users/:user_id/posts/:id/comments' => 'comments#create', as: :comment
+
+  #likes
+  get 'users/:user_id/posts/:id/likes/new' => 'likes#create', as: :likes_create
  
 end
