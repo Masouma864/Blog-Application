aed91317f4bc466f3299b54809093bf0f3ccd96d
masouma <masouma2020@gmail.com>
2023-02-15 14:26:36 +0430

    configure database-cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,9 +6,9 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'capybara'
-require 'support/database_cleaner'
-# Add additional requires below this line. Rails is not loaded until this point!
 
+# Add additional requires below this line. Rails is not loaded until this point!
+require 'support/database_cleaner'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
